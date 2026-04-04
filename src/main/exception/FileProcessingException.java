27ea2c761d9cdf75144e9f6dcc2ddae58cd34248
elisabetta-turcan <elisabetta.turcan@gmail.com>
2026-04-04 19:34:40 +0300

    [caesar-cipher-cli] new exception text

--- src/main/exception/FileProcessingException.java
+++ src/main/exception/FileProcessingException.java
@@ -1,7 +1,7 @@
 package main.exception;
 
 public class FileProcessingException extends RuntimeException {
-    private static final String ERROR_MESSAGE = "Файл не найден";
+    private static final String ERROR_MESSAGE = "Не удалось прочитать файл";
 
     public FileProcessingException() {
         super(ERROR_MESSAGE);
